bf34e87ee87fe080ee1faf4fec9a53c87a05c7e4
Asael <aatello@pichincha.com>
2024-06-07 23:05:52 -0500

    add step 3

---
+++ src/app/pages/step-3-passenger-info/passenger-info/passenger-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passenger-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './passenger-info.component.html',
+  styleUrl: './passenger-info.component.scss'
+})
+export class PassengerInfoComponent {
+
+}
