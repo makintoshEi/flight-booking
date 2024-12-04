8ddaa63834d91856c3caded1610c7b3fcc968181
Asael <makintosh_ragga@yahoo.es>
2024-12-04 11:51:50 -0500

    feat: remove styleUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.component.html'
 })
 export class AppComponent { }
